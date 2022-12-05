654baaa2b052d98d5ae01f5ca51400461f6fc0fb
USER <USER@DESKTOP-9VIO7SL>
2022-12-05 02:55:16 -0800

    Add project files.

---
+++ Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace Core.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<T> FindAsync(Expression<Func<T, bool>> criteria);
+        Task<TType> FindAsync<TType>(Expression<Func<T, bool>> criteria, Expression<Func<T, TType>> select);
+        Task<List<T>> ListAllAsync();
+        Task<List<T>> ListAllAsync(Expression<Func<T, object>> include);
+        Task<List<T>> ListAllAsync(string[] includes);
+        Task<List<T>> ListAllAsync(Expression<Func<T, bool>> criteria);
+        Task<List<T>> ListAllAsync(Expression<Func<T, bool>> criteria, Expression<Func<T, bool>> secondCriteria);
+        Task<ICollection<TType>> ListAllAsync<TType>(Expression<Func<T, TType>> select);
+        Task<ICollection<TType>> ListAllAsync<TType>(Expression<Func<T, bool>> criteria, Expression<Func<T, TType>> select);
+
+        Task AddAsync(T entity);
+        void Update(T entity);
+        bool Delete(T entity);
+    }
+}
---
+++ Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,104 @@
+﻿using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Infrastructure.Context;
+
+namespace Infrastructure.Repositories
+{
+    public class BaseRepository<T>: IBaseRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        public BaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<T> FindAsync(Expression<Func<T, bool>> criteria)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            return await query.SingleOrDefaultAsync(criteria);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<List<T>> ListAllAsync(Expression<Func<T, object>> include)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            return await query.Include(include).ToListAsync();
+        }
+
+        public async Task<List<T>> ListAllAsync(string[] includes)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            foreach (var item in includes)
+            {
+                query = query.Include(item);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<List<T>> ListAllAsync(Expression<Func<T, bool>> criteria)
+        {
+            return await _context.Set<T>().Where(criteria).ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public async Task<List<T>> ListAllAsync(Expression<Func<T, bool>> criteria
+            , Expression<Func<T, bool>> secondCriteria)
+        {
+            var query = _context.Set<T>().Where(criteria);
+            query = query.Where(secondCriteria);
+
+            return await query.ToListAsync();
+        }
+
+        public bool Delete(T entity)
+        {
+            var result = _context.Remove(entity);
+            if (result == null)
+            {
+                return false;
+            }
+            return true;
+        }
+        public async Task<ICollection<TType>> ListAllAsync<TType>(Expression<Func<T, TType>> select)
+        {
+            var query = _context.Set<T>().Select(select);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<ICollection<TType>> ListAllAsync<TType>(Expression<Func<T, bool>> criteria, Expression<Func<T, TType>> select)
+        {
+            var query = _context.Set<T>()
+                .Where(criteria).Select(select);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<TType> FindAsync<TType>(Expression<Func<T, bool>> criteria, Expression<Func<T, TType>> select)
+        {
+            var query = _context.Set<T>()
+                .Where(criteria).Select(select);
+
+            return await query.FirstOrDefaultAsync();
+        }
+    }
+}
